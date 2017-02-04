51251747909a87b6aba114ad2b7a86b67b325f9c
hccde <1076663958@qq.com>
2017-02-04 10:08:30 +0800

    fixed untracked files

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 import { ajax } from './lib/net'
 import { MD5 } from './lib/md5'
+//your account info
+import account from './account'
+let template = require('./template.html');
+console.log(template);
 
+let query_word = 'test';
 let query_str = {
-	q: 'test',
-	appid: 'your appid',
-	salt: 'random number',
+	q: query_word,
+	appid: account.appid,
+	salt: Math.floor(Math.random()*100),
 	from: 'en',
 	to: 'zh',
 	sign: 'your sign',
 	callback:'callback'
-}
+};
 //compute
-query_str.sign = ''
+query_str.sign = MD5(account.appid+query_word+query_str.salt+account.password);
 
-function callback(res){
+window.callback = function(res){
 	//response callback
+	console.log(res);
 }
 
-ajax(query_str)
+// ajax(query_str)
