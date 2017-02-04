d82687a36cf1e878f5bc4245d6f27bf6c58cb8a0
hccde <1076663958@qq.com>
2017-02-04 14:29:41 +0800

    model class get translation result

--- index.js
+++ index.js
@@ -1,26 +1,6 @@
-import { ajax } from './lib/net'
-import { MD5 } from './lib/md5'
-//your account info
-import account from './account'
+import { Model } from './lib/Model.class'
 let template = require('./template.html');
-console.log(template);
 
-let query_word = 'test';
-let query_str = {
-	q: query_word,
-	appid: account.appid,
-	salt: Math.floor(Math.random()*100),
-	from: 'en',
-	to: 'zh',
-	sign: 'your sign',
-	callback:'callback'
-};
-//compute
-query_str.sign = MD5(account.appid+query_word+query_str.salt+account.password);
-
-window.callback = function(res){
-	//response callback
-	console.log(res);
-}
-
+let m = new Model('stall')
+m.bind(template)
 // ajax(query_str)
